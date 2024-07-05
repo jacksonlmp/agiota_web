2f19efffb1bca26570c475d9a84e1c6e0c94b235
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2024-07-05 01:00:30 -0300

    Criação do projeto

---
+++ src/main/java/br/edu/ufape/agiota/AgiotaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.agiota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiotaApplication.class, args);
+	}
+
+}
