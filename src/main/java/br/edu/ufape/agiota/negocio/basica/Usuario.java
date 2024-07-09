b6f7342a9bcce002076abcbd771d1c706b62b9db
luiz-davi-m <luizd4398@gmail.com>
2024-07-08 22:27:12 -0300

    criando classe abstrata de cliente

--- src/main/java/br/edu/ufape/agiota/negocio/basica/Usuario.java
+++ src/main/java/br/edu/ufape/agiota/negocio/basica/Usuario.java
@@ -1,4 +1,27 @@
 package br.edu.ufape.agiota.negocio.basica;
 
-public class Usuario {
+import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+    private String cpf;
+    private String reputacao;
+
+    @OneToOne
+    @Cascade(CascadeType.ALL)
+    private Endereco endereco;
 }
