8d91b8282eb396c83b1cb5eaf9b3bf3f56f2d7fb
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-07-11 01:15:02 -0300

    Arquivo DTO Agiota

--- src/main/java/br/edu/ufape/agiota/dtos/AgiotaDTO.java
+++ src/main/java/br/edu/ufape/agiota/dtos/AgiotaDTO.java
@@ -1,30 +1,56 @@
 package br.edu.ufape.agiota.dtos;
 
+import br.edu.ufape.agiota.negocio.basica.Agiota;
+import br.edu.ufape.agiota.negocio.basica.Cliente;
 import br.edu.ufape.agiota.negocio.basica.Usuario;
 import br.edu.ufape.agiota.negocio.basica.enums.PeriodoTaxa;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jdk.jfr.Name;
 import lombok.Data;
 
-@Entity
 @Data
 public class AgiotaDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-
-    @NotNull
+    @NotBlank
     private String nome;
+
+    @NotBlank
     private String email;
     private String senha;
+
+    @NotBlank
     private String telefone;
+
+    @NotBlank
     private String cpf;
+
+    @NotBlank
     private String reputacao;
+
+    @NotBlank
     private Double taxaDeJuros;
+
+    @NotBlank
     private String metodoCobranca;
+
+    @NotBlank
     private PeriodoTaxa periodoTaxa;
+
+    @NotNull
+    private EnderecoDTO endereco;
+
+    public void toAgiota(Agiota agiota) {
+        agiota.setNome(getNome());
+        agiota.setEmail(getEmail());
+        agiota.setSenha(getSenha());
+        agiota.setTelefone(getTelefone());
+        agiota.setCpf(getCpf());
+        agiota.setTaxaDeJuros(getTaxaDeJuros());
+        agiota.setMetodoCobranca(getMetodoCobranca());
+        agiota.setPeriodoTaxa(getPeriodoTaxa());
+    }
 }
