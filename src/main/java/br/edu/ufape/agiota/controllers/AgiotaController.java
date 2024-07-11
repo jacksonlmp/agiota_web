90031712e806c59b8bfa2ea7df1c7460b87fde89
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-07-11 02:53:47 -0300

    Atualização de métodos de Agiota

--- src/main/java/br/edu/ufape/agiota/controllers/AgiotaController.java
+++ src/main/java/br/edu/ufape/agiota/controllers/AgiotaController.java
@@ -1,56 +1,59 @@
 package br.edu.ufape.agiota.controllers;
 
+import br.edu.ufape.agiota.dtos.AgiotaDTO;
+import br.edu.ufape.agiota.fachada.Fachada;
+import br.edu.ufape.agiota.fachada.exceptions.RegistroNaoEncontradoException;
+import br.edu.ufape.agiota.fachada.exceptions.SenhaNulaException;
 import br.edu.ufape.agiota.negocio.basica.Agiota;
-import br.edu.ufape.agiota.negocio.services.AgiotaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/agiotas")
 public class AgiotaController {
     @Autowired
-    private AgiotaService agiotaService;
+    private Fachada fachada;
 
     @PostMapping
-    public Agiota criarAgiota(@RequestBody Agiota agiota) throws Exception{
-        return agiotaService.saveAgiota(agiota);
+    public ResponseEntity<?> criarAgiota(@RequestBody @Valid AgiotaDTO agiotaDTO) throws RegistroNaoEncontradoException, SenhaNulaException {
+        try {
+            Agiota agiota = fachada.criarAgiota(agiotaDTO);
+            return ResponseEntity.ok().body(agiota);
+        } catch (RegistroNaoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (SenhaNulaException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
     @GetMapping
-    public List<Agiota> buscarTodosAgiotas() {
-        return agiotaService.getAllAgiotas();
+    public List<Agiota> listarAgiotas() {
+        return fachada.listarAgiotas();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Agiota> buscarAgiotaPorId(@PathVariable Integer id) {
-        Optional<Agiota> agiota = agiotaService.getAgiotaById(id);
-        return agiota.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    public ResponseEntity<?> buscarAgiota(@PathVariable long id) throws RegistroNaoEncontradoException {
+        try {
+            Agiota agiota = fachada.buscarAgiota(id);
+            return ResponseEntity.ok().body(agiota);
+        } catch (RegistroNaoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Agiota> atualizarAgiota(@PathVariable Integer id, @RequestBody Agiota agiotaDetails) {
-        Optional<Agiota> optionalAgiota = agiotaService.getAgiotaById(id);
-        if (optionalAgiota.isPresent()) {
-            Agiota agiota = optionalAgiota.get();
-            agiota.setNome(agiotaDetails.getNome());
-            agiota.setEmail(agiotaDetails.getEmail());
-            agiota.setSenha(agiotaDetails.getSenha());
-            agiota.setTelefone(agiotaDetails.getTelefone());
-            agiota.setCpf(agiotaDetails.getCpf());
-            agiota.setReputacao(agiotaDetails.getReputacao());
-            agiota.setEndereco(agiotaDetails.getEndereco());
-            agiota.setTaxaDeJuros(agiotaDetails.getTaxaDeJuros());
-            agiota.setMetodoCobranca(agiotaDetails.getMetodoCobranca());
-            agiota.setPeriodoTaxa(agiotaDetails.getPeriodoTaxa());
-
-            final Agiota updatedAgiota = agiotaService.saveAgiota(agiota);
-            return ResponseEntity.ok(updatedAgiota);
-        } else {
-            return ResponseEntity.notFound().build();
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<?> atualizarAgiota(@RequestBody @Valid AgiotaDTO agiotaDTO, @PathVariable long id) throws Exception  {
+        try {
+            Agiota agiota = fachada.atualizarAgiota(agiotaDTO, id);;
+            return ResponseEntity.ok().body(agiota);
+        } catch (RegistroNaoEncontradoException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
+
