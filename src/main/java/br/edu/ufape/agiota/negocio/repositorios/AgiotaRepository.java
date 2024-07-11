ee60103b3de1d2dade723e77bddab99d5de97550
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-07-11 02:53:32 -0300

    Mudando tipo na interface e metodo de email

--- src/main/java/br/edu/ufape/agiota/negocio/repositorios/AgiotaRepository.java
+++ src/main/java/br/edu/ufape/agiota/negocio/repositorios/AgiotaRepository.java
@@ -3,5 +3,6 @@ package br.edu.ufape.agiota.negocio.repositorios;
 import br.edu.ufape.agiota.negocio.basica.Agiota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgiotaRepository extends JpaRepository<Agiota, Integer> {
+public interface AgiotaRepository extends JpaRepository<Agiota, Long> {
+    public Agiota findByEmail(String email);
 }
