acc0cc4828ea478482c71ddca61eb52d3bccf31f
luiz-davi-m <163929433+luiz-davi-m@users.noreply.github.com>
2024-07-10 22:00:14 -0300

    Update src/main/java/br/edu/ufape/agiota/negocio/services/interfaces/UsuarioServiceInterface.java
    Co-authored-by: Thiago Cavalcanti <tcavalcanti.tc@gmail.com>

--- src/main/java/br/edu/ufape/agiota/negocio/services/interfaces/UsuarioServiceInterface.java
+++ src/main/java/br/edu/ufape/agiota/negocio/services/interfaces/UsuarioServiceInterface.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface UsuarioServiceInterface {
     List<Cliente> usuarios();
 
-    Cliente create(Cliente cliente) throws Exception;
+    Cliente criarCliente(Cliente cliente) throws Exception;
 }
