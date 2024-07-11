9dc83678d98ee979c0e8b301927f003bd3ee171a
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2024-07-11 02:14:48 -0300

    Ajustes no CRUD de cliente

--- src/main/java/br/edu/ufape/agiota/negocio/repositorios/ClienteRepository.java
+++ src/main/java/br/edu/ufape/agiota/negocio/repositorios/ClienteRepository.java
@@ -1,11 +1,10 @@
 package br.edu.ufape.agiota.negocio.repositorios;
 
 import br.edu.ufape.agiota.negocio.basica.Cliente;
-import br.edu.ufape.agiota.negocio.basica.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    public Cliente findByEmail(String email);
+    Cliente findByEmail(String email);
 }
