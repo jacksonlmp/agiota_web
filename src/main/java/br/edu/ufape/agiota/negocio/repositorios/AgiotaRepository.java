7d3e4778fc5bceb4932a95eff9dae29331a8e0df
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-07-11 10:51:54 -0300

    Refactor

--- src/main/java/br/edu/ufape/agiota/negocio/repositorios/AgiotaRepository.java
+++ src/main/java/br/edu/ufape/agiota/negocio/repositorios/AgiotaRepository.java
@@ -3,6 +3,6 @@ package br.edu.ufape.agiota.negocio.repositorios;
 import br.edu.ufape.agiota.negocio.basica.Agiota;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface AgiotaRepository extends JpaRepository<Agiota, Long> {
-    public Agiota findByEmail(String email);
+public interface AgiotaRepository extends JpaRepository<Agiota, Long> {
+    Agiota findByEmail(String email);
 }
