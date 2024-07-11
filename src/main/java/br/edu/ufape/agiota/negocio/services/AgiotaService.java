b31583192331c98372ff88fc37f6133b76115a52
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-07-11 02:54:41 -0300

    Adicionando updates de endereco e atualização de metodos de Agiota

--- src/main/java/br/edu/ufape/agiota/negocio/services/AgiotaService.java
+++ src/main/java/br/edu/ufape/agiota/negocio/services/AgiotaService.java
@@ -1,28 +1,78 @@
 package br.edu.ufape.agiota.negocio.services;
 
+import br.edu.ufape.agiota.dtos.AgiotaDTO;
+import br.edu.ufape.agiota.dtos.ClienteDTO;
+import br.edu.ufape.agiota.fachada.exceptions.RegistroNaoEncontradoException;
+import br.edu.ufape.agiota.fachada.exceptions.SenhaNulaException;
 import br.edu.ufape.agiota.negocio.basica.Agiota;
+import br.edu.ufape.agiota.negocio.basica.Cliente;
+import br.edu.ufape.agiota.negocio.basica.Endereco;
 import br.edu.ufape.agiota.negocio.repositorios.AgiotaRepository;
+import br.edu.ufape.agiota.negocio.repositorios.EnderecoRepository;
+import br.edu.ufape.agiota.negocio.services.interfaces.AgiotaServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 @Service
-public class AgiotaService {
+public class AgiotaService implements AgiotaServiceInterface {
     @Autowired
     private AgiotaRepository agiotaRepository;
 
-    public Agiota saveAgiota(Agiota agiota) {
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    public Agiota criarAgiota(AgiotaDTO agiotaDTO) throws RegistroNaoEncontradoException, SenhaNulaException {
+        if (nonNull(buscarAgiotaPorEmail(agiotaDTO.getEmail()))) {
+            throw new RegistroNaoEncontradoException("O email informado já se encontra cadastrado no sistema");
+        }
+
+        if (isNull(agiotaDTO.getSenha())) {
+            throw new SenhaNulaException("Senha é um campo obrigatório");
+        }
+
+        Endereco endereco = new Endereco();
+        agiotaDTO.getEndereco().toEndereco(endereco);
+        Endereco novoEndereco = enderecoRepository.save(endereco);
+
+        Agiota agiota = new Agiota();
+        agiotaDTO.toAgiota(agiota);
+        agiota.setEndereco(novoEndereco);
+
         return agiotaRepository.save(agiota);
     }
 
-    public List<Agiota> getAllAgiotas() {
+    public Agiota buscarAgiota(long id) throws RegistroNaoEncontradoException {
+        Optional<Agiota> clienteOpt = agiotaRepository.findById(id);
+
+        if (clienteOpt.isPresent()) return clienteOpt.get();
+
+        throw new RegistroNaoEncontradoException("Agiota com o identificador " + id + " não foi encontrado!");
+    }
+
+    @Override
+    public Agiota atualizarAgiota(AgiotaDTO agiotaDTO, long id) throws RegistroNaoEncontradoException {
+        Agiota agiota = buscarAgiota(id);
+
+        agiotaDTO.getEndereco().toEndereco(agiota.getEndereco());
+        enderecoRepository.save(agiota.getEndereco());
+
+        agiotaDTO.toAgiota(agiota);
+
+        return agiotaRepository.save(agiota);
+    }
+
+    public List<Agiota> listarAgiotas() {
         return agiotaRepository.findAll();
     }
 
-    public Optional<Agiota> getAgiotaById(Integer id) {
-        return agiotaRepository.findById(id);
+    private Agiota buscarAgiotaPorEmail(String email){
+        return agiotaRepository.findByEmail(email);
     }
 
 }
