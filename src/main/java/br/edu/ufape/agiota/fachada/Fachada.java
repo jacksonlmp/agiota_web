d9a13b3521d45b1630956160004e794cb7fe8e9d
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-07-11 02:52:17 -0300

    Atualizando fachada com Agiota

--- src/main/java/br/edu/ufape/agiota/fachada/Fachada.java
+++ src/main/java/br/edu/ufape/agiota/fachada/Fachada.java
@@ -1,15 +1,17 @@
 package br.edu.ufape.agiota.fachada;
 
+import br.edu.ufape.agiota.dtos.AgiotaDTO;
 import br.edu.ufape.agiota.dtos.ClienteDTO;
 import br.edu.ufape.agiota.fachada.exceptions.RegistroNaoEncontradoException;
 import br.edu.ufape.agiota.fachada.exceptions.SenhaNulaException;
+import br.edu.ufape.agiota.negocio.basica.Agiota;
 import br.edu.ufape.agiota.negocio.basica.Cliente;
+import br.edu.ufape.agiota.negocio.services.interfaces.AgiotaServiceInterface;
 import br.edu.ufape.agiota.negocio.services.interfaces.ClienteServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class Fachada {
@@ -30,5 +32,23 @@ public class Fachada {
 
     public Cliente atualizarCliente(ClienteDTO clienteDTO, long id) throws RegistroNaoEncontradoException {
         return clienteService.atualizarCliente(clienteDTO, id);
+    }
+
+    //Agiota
+    @Autowired
+    private AgiotaServiceInterface agiotaService;
+    public List<Agiota> listarAgiotas() {
+        return agiotaService.listarAgiotas();
+    }
+    public Agiota criarAgiota(AgiotaDTO agiotaDTO) throws RegistroNaoEncontradoException, SenhaNulaException {
+        return agiotaService.criarAgiota(agiotaDTO);
+    }
+
+    public Agiota buscarAgiota(long id) throws RegistroNaoEncontradoException {
+        return agiotaService.buscarAgiota(id);
+    }
+
+    public Agiota atualizarAgiota(AgiotaDTO agiotaDTO, long id) throws RegistroNaoEncontradoException {
+        return agiotaService.atualizarAgiota(agiotaDTO, id);
     }
 }
