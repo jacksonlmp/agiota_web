68b4d1fb0bf30f6f6aad510fc92330e1543507be
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2024-07-12 01:36:20 -0300

    Criada configuração de segurança

--- src/main/java/br/edu/ufape/agiota/negocio/basica/Usuario.java
+++ src/main/java/br/edu/ufape/agiota/negocio/basica/Usuario.java
@@ -2,17 +2,22 @@ package br.edu.ufape.agiota.negocio.basica;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import lombok.Data;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Inheritance(strategy = InheritanceType.JOINED)
 @EqualsAndHashCode(of = "id")
-public abstract class Usuario {
+public abstract class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -30,4 +35,39 @@ public abstract class Usuario {
     @OneToOne
     @Cascade(CascadeType.ALL)
     private Endereco endereco;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
