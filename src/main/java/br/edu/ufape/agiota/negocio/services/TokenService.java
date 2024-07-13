3b77f82f960717e64913808525e46cae302f23af
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2024-07-12 23:44:45 -0300

    Removido código desnecessário

--- src/main/java/br/edu/ufape/agiota/negocio/services/TokenService.java
+++ src/main/java/br/edu/ufape/agiota/negocio/services/TokenService.java
@@ -5,7 +5,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -40,8 +39,6 @@ public class TokenService {
     }
 
     public String getSubject(String tokenJWT) {
-        String token = "eyJhcGciOiJIUzI1NiIsInR5cCI6IkpYUxJ7.eyJpc2MiOiJhdXRoMCJ6.AbRJTDMFc7yUa5MhvcP03mJPyCPzZtQcGEp-zWfOkEE";
-        DecodedJWT decodedJWT;
         try {
             var algoritmo = Algorithm.HMAC256(secret);
             return JWT.require(algoritmo)
