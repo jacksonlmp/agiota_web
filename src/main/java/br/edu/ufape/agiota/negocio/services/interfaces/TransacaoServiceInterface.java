cfbe235e89651d8f34829a29b3b17bc1a6e26c38
yasmiinmuniz <yasmindasilvamuniz@gmail.com>
2024-07-16 19:24:53 -0300

    Substituir exceção de criarTransacao pela RegistroJaExistenteException

--- src/main/java/br/edu/ufape/agiota/negocio/services/interfaces/TransacaoServiceInterface.java
+++ src/main/java/br/edu/ufape/agiota/negocio/services/interfaces/TransacaoServiceInterface.java
@@ -1,13 +1,14 @@
 package br.edu.ufape.agiota.negocio.services.interfaces;
 
 import br.edu.ufape.agiota.dtos.TransacaoDTO;
+import br.edu.ufape.agiota.fachada.exceptions.RegistroJaExistenteException;
 import br.edu.ufape.agiota.fachada.exceptions.RegistroNaoEncontradoException;
 import br.edu.ufape.agiota.negocio.basica.Transacao;
 
 import java.util.List;
 
 public interface TransacaoServiceInterface {
-    Transacao criarTransacao(TransacaoDTO transacaoDTO) throws RegistroNaoEncontradoException;
+    Transacao criarTransacao(TransacaoDTO transacaoDTO) throws RegistroJaExistenteException;
     List<Transacao> buscarTransacoesPorParcela(long idParcela) throws RegistroNaoEncontradoException;
     Transacao buscarTransacao(long idTransacao) throws RegistroNaoEncontradoException;
     List<Transacao> listarTransacoes();
