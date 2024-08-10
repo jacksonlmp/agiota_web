874626dbe8e4e3ce12fb27ca8137161f0b92e466
yasmiinmuniz <yasmindasilvamuniz@gmail.com>
2024-08-10 17:52:50 -0300

    Correções em lembrete

--- src/main/java/br/edu/ufape/agiota/dtos/LembreteDTO.java
+++ src/main/java/br/edu/ufape/agiota/dtos/LembreteDTO.java
@@ -1,22 +1,28 @@
 package br.edu.ufape.agiota.dtos;
 
-import br.edu.ufape.agiota.negocio.basica.Lembrete;
-import lombok.Data;
-
 import java.util.Date;
+
+import br.edu.ufape.agiota.negocio.basica.Lembrete;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
 
 @Data
 public class LembreteDTO {
+    @NotNull(message = "A data não pode ser nula.")
     private Date data;
+
+    @NotBlank(message = "O texto do lembrete não pode estar em branco.")
     private String texto;
+
+    @Positive(message = "O ID da parcela deve ser um número positivo.")
     private long parcelaId;
-    private long usuarioId; 
     
-    public Lembrete toEntity() {
-        Lembrete lembrete = new Lembrete();
-        lembrete.setData(this.data);
-        lembrete.setTexto(this.texto);
-        lembrete.setUsuarioId(this.usuarioId);
-        return lembrete;
-    }
+    public Lembrete toLembrete() {  
+        Lembrete lembrete = new Lembrete();  
+        lembrete.setData(getData());  
+        lembrete.setTexto(getTexto());  
+        return lembrete;  
+    } 
 }
