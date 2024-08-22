18ae48a6fa6613d37e2e581629432f6d1d0ad936
yasmiinmuniz <yasmindasilvamuniz@gmail.com>
2024-08-21 21:30:35 -0300

    Refatoração das funções de registro e login de usuários para usar Axios
    - Atualizado a URL da API para `http://localhost:8080`.
    - Alterado o método de comunicação com a API de `fetch` para `axios`.
    - Adicionada validação de dados na função `registerUser` para verificar a presença de campos obrigatórios e campos específicos para diferentes tipos de usuários (`cliente` e `agiota`).
    - Atualizada a função `registerUser` para enviar dados para o endpoint `/cadastro` e melhorado o tratamento de erros.
    - Alterada a função `loginUser` para enviar a solicitação de login via `GET` com parâmetros e melhorar o tratamento de erros.

--- agiota_front_end/src/api.js
+++ agiota_front_end/src/api.js
@@ -1,25 +1,39 @@
-const API_URL = 'http://localhost:3000/users';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
 
 export const registerUser = async (user) => {
-    const response = await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-    });
-    if (!response.ok) {
-        throw new Error('Erro ao registrar o usuário1');
+  try {
+    if (!user.nome || !user.email || !user.senha || !user.telefone || !user.cpf || !user.endereco) {
+      throw new Error('Dados incompletos');
     }
-    return await response.json();
+
+    if (user.role === 'cliente' && (!user.profissao || !user.localDeTrabalho)) {
+      throw new Error('Dados do cliente incompletos');
+    }
+
+    if (user.role === 'agiota' && (!user.taxaDeJuros || !user.metodoCobranca || !user.periodoTaxa)) {
+      throw new Error('Dados do agiota incompletos');
+    }
+
+    const response = await axios.post(`${API_URL}/cadastro`, user);
+    console.log('Usuário cadastrado:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao cadastrar:', error.response ? error.response.data : error.message);
+    throw error;
+  }
 };
 
-export const loginUser = async (email, senha) => {
-    const response = await fetch(`${API_URL}?email=${email}&senha=${senha}`);
-    const users = await response.json();
-    if (users.length > 0) {
-        return users[0]; // Retorna o usuário correspondente
-    } else {
-        throw new Error('Credenciais inválidas');
-    }
+export const loginUser = async (username, password) => {
+  try {
+    const response = await axios.get(`${API_URL}/login`, {
+      params: { username, password },
+    });
+    console.log('Login bem-sucedido:', response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Erro no login:', error.response ? error.response.data : error.message);
+    throw error;
+  }
 };
