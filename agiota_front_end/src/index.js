86b1c50977d4df64917fd655e4beffcf3c62eedb
yasmiinmuniz <yasmindasilvamuniz@gmail.com>
2024-08-21 21:34:20 -0300

    Atualização das rotas da aplicação para incluir Login e Cadastro
    - Adicionadas rotas para os componentes `Login` e `Cadastro`.
    - Atualizada a configuração do roteador para utilizar o `createBrowserRouter` e `RouterProvider`.
    - Removida a rota para `/emprestimos` e comentada a rota para a página inicial.
    - Corrigida a definição das rotas para `Login` e `Cadastro` removendo o caminho relativo incorreto.

--- agiota_front_end/src/index.js
+++ agiota_front_end/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Emprestimos from "./routes/Emprestimos";
-import {createBrowserRouter, RouterProvider, Route} from "react-router-dom";
+import Login from "./components/login";
+import Cadastro from "./components/cadastro";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,11 +12,12 @@ const router = createBrowserRouter([
     {
         element: <App/>,
         children: [
-            { path: "/" },
-            { path: "/emprestimos", element: <Emprestimos /> }
+            // { path: "/", element: <Home /> },
+            { path: "./components/login", element: <Login /> },
+            { path: "./components/cadastro", element: <Cadastro /> }
         ]
     }
-])
+]);
 
 root.render(
   <React.StrictMode>
