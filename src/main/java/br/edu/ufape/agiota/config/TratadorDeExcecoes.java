11c8bb230636ddabc48cc45f3fb364494d8e32f5
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2024-09-20 23:08:06 -0300

    fix[Parcela]: ajustado cálculo de juros ao criar parcela

--- src/main/java/br/edu/ufape/agiota/config/TratadorDeExcecoes.java
+++ src/main/java/br/edu/ufape/agiota/config/TratadorDeExcecoes.java
@@ -1,6 +1,7 @@
 package br.edu.ufape.agiota.config;
 
 import br.edu.ufape.agiota.fachada.exceptions.OperacaoNaoPermitidaException;
+import br.edu.ufape.agiota.fachada.exceptions.RegistroNaoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,5 +23,17 @@ public class TratadorDeExcecoes {
         erro.put("erro", resposta);
 
         return new ResponseEntity<>(erro, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(RegistroNaoEncontradoException.class)
+    protected ResponseEntity<Object> tratarExcecaoRegistroNaoEncontrado(RegistroNaoEncontradoException ex) {
+        Map<String, String> resposta = new HashMap<>();
+        resposta.put("tipo", "RegistroNaoEncontrado");
+        resposta.put("mensagem", ex.getMessage());
+
+        Map<String, Object> erro = new HashMap<>();
+        erro.put("erro", resposta);
+
+        return new ResponseEntity<>(erro, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/br/edu/ufape/agiota/dtos/EmprestimoClienteDTO.java
+++ src/main/java/br/edu/ufape/agiota/dtos/EmprestimoClienteDTO.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 @Data
 public class EmprestimoClienteDTO {
@@ -23,12 +24,27 @@ public class EmprestimoClienteDTO {
     @Positive
     private long agiotaId;
 
+    @NotNull
+    private Date dataEmprestimo;
+
+    @NotNull
+    private Date dataDeVencimentoInicial;
+
+    @Positive
+    @Min(1)
+    private int quantidadeParcelas;
+
+    @NotNull
+    @Positive
+    private int periodoParcelas;
+
     public void toSolicitarEmprestimo(Emprestimo emprestimo, Cliente cliente, Agiota agiota) {
         emprestimo.setValorEmprestimo(getValorEmprestimo());
         emprestimo.setGarantia(getGarantia());
         emprestimo.setStatus(StatusEmprestimo.AGUARDANDO_APROVACAO);
         emprestimo.setCliente(cliente);
         emprestimo.setAgiota(agiota);
+        emprestimo.setTaxaJuros(agiota.getTaxaDeJuros());
     }
 
 }
