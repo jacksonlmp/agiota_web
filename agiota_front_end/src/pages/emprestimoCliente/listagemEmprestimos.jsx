e00ca1dadc8f9e4d3e27d04d19b9f1f1e851fc41
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-09-25 22:07:12 -0300

    Adicionando redirecionamento na tabela de listagem de emprestimos

--- agiota_front_end/src/pages/emprestimoCliente/listagemEmprestimos.jsx
+++ agiota_front_end/src/pages/emprestimoCliente/listagemEmprestimos.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import {DataGrid} from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
+import { useNavigate } from 'react-router-dom';
 
 function ListagemEmprestimos() {
     const [emprestimos, setEmprestimos] = useState([]);
     const user = JSON.parse(localStorage.getItem('@Auth:user'));
+    const navigate = useNavigate();
+
     const columns = [
         { field: 'id', headerName: 'ID', width: 50 },
         { field: 'garantia', headerName: 'Garantia', width: 130 },
@@ -14,6 +17,28 @@ function ListagemEmprestimos() {
         { field: 'dataDeVencimentoInicial', headerName: 'Data de Vencimento', width: 250 },
         { field: 'quantidadeParcelas', headerName: 'Nº Parcelas', width: 110 },
         { field: 'periodoParcelas', headerName: 'Periodo', width: 80 },
+        {
+            field: 'acoes',
+            headerName: 'Ações',
+            width: 150,
+            renderCell: (params) => (
+                <button
+                    onClick={() => navigate(`/app/listar-parcelas/${params.row.id}`)}
+                    style={{ 
+                        backgroundColor: '#007bff',
+                        color: 'white', 
+                        border: 'none', 
+                        borderRadius: '8px', 
+                        cursor: 'pointer', 
+                        display: 'flex', 
+                        padding: '0px 10px',
+                        fontSize: '0.8rem' 
+                    }}
+                >
+                    Ver Parcelas
+                </button>
+            ),
+        },
     ];
 
     useEffect(() => {
@@ -22,7 +47,7 @@ function ListagemEmprestimos() {
                 const response = await axios.get(`http://localhost:8080/${user.usuario.tipo.toLowerCase()}/emprestimos`, {
                     headers: {
                         'Authorization': `Bearer ${user.token}`
-                      }
+                    }
                 });
                 setEmprestimos(response.data);
             } catch (error) {
@@ -40,6 +65,7 @@ function ListagemEmprestimos() {
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
+                getRowId={(row) => row.id}
             />
         </div>
     );
