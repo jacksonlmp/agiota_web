02eddd680d3ab00e12605cdef0248337920b173a
jacksonlmp <jacksonlmp17.jl@gmail.com>
2024-09-25 21:15:43 -0300

    Rota

--- agiota_front_end/src/index.js
+++ agiota_front_end/src/index.js
@@ -9,6 +9,7 @@ import ListagemEmprestimos from "./pages/emprestimoCliente/listagemEmprestimos";
 import Agiotas from "./pages/agiota/listagemAgiotas";
 import Clientes from "./pages/cliente/listagemClientes";
 import CadastroCliente from "./pages/cliente/cadastroCliente";
+import ListagemParcelas from "./pages/parcela/listagemParcela"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
             {
                 path: "cadastrar-cliente",
                 element: <CadastroCliente />
+            },
+            {
+                path: "listar-parcelas/:emprestimoId",
+                element: <ListagemParcelas />
             }
         ]
     }
