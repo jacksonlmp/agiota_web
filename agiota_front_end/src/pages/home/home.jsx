1cb11b2d5bf382551149a68b8ced156bf53a555c
yasmiinmuniz <yasmindasilvamuniz@gmail.com>
2024-09-25 23:39:52 -0300

    feat: criar componente Home

--- agiota_front_end/src/pages/home/home.jsx
+++ agiota_front_end/src/pages/home/home.jsx
@@ -1,39 +1,41 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom'; // Para navegação
-import { BsPerson as IconAgiota, BsCash as IconDinheiro } from "react-icons/bs"; // Alterando para ícone de dinheiro
+import { useNavigate, Link } from 'react-router-dom';
+import { BsPerson as IconAgiota, BsCash as IconDinheiro } from "react-icons/bs";
+
+const CardItem = ({ to, icon: Icon, title }) => (
+    <Link to={to} className="w-full">
+        <div className="h-[180px] flex flex-col justify-between p-4 bg-gradient-to-r from-blue-400 to-green-500 hover:from-blue-500 hover:to-green-600 rounded-lg shadow-md transition-transform transform hover:scale-105 duration-300">
+            <div className="flex flex-col items-center flex-grow">
+                <Icon className="text-white w-16 h-16" />
+                <h2 className="mt-2 text-center text-lg font-semibold text-white">{title}</h2>
+            </div>
+        </div>
+    </Link>
+);
 
 function Home() {
     const navigate = useNavigate();
 
-    const handleNavigate = (path) => {
-        navigate(path);
-    };
-
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-100 to-gray-200 p-6">
-            <h1 className="text-3xl font-bold mb-8 text-gray-800">Bem-vindo ao Sistema de Empréstimos</h1>
-            <div className="flex gap-6 w-full max-w-6xl">
-                <button
-                    onClick={() => handleNavigate("/app/emprestimos")}
-                    className="flex-grow bg-gradient-to-r from-blue-400 to-green-500 hover:from-blue-500 hover:to-green-600 text-white font-semibold py-5 px-10 rounded-lg shadow-md flex items-center transition-transform transform hover:scale-105 duration-300 focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50"
-                >
-                    <IconDinheiro className="mr-2 w-6 h-6" /> {/* Ícone de dinheiro */}
-                    Ver Empréstimos
-                </button>
-                <button
-                    onClick={() => handleNavigate("/app/agiotas")}
-                    className="flex-grow bg-gradient-to-r from-blue-400 to-green-500 hover:from-blue-500 hover:to-green-600 text-white font-semibold py-5 px-10 rounded-lg shadow-md flex items-center transition-transform transform hover:scale-105 duration-300 focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50"
-                >
-                    <IconAgiota className="mr-2 w-6 h-6" /> {/* Ícone de pessoa */}
-                    Ver Agiotas
-                </button>
-                <button
-                    onClick={() => handleNavigate("/app/solicitar-emprestimo")}
-                    className="flex-grow bg-gradient-to-r from-blue-400 to-green-500 hover:from-blue-500 hover:to-green-600 text-white font-semibold py-5 px-10 rounded-lg shadow-md flex items-center transition-transform transform hover:scale-105 duration-300 focus:outline-none focus:ring-4 focus:ring-green-500 focus:ring-opacity-50"
-                >
-                    <IconDinheiro className="mr-2 w-6 h-6" /> {/* Ícone de dinheiro */}
-                    Solicitar Empréstimo
-                </button>
+        <div className="flex flex-col items-center justify-start h-screen bg-white p-6">
+            {/* Título posicionado no topo */}
+            <h1 className="text-3xl font-bold mb-8 text-gray-800 text-center mt-4">Bem-vindo ao Sistema de Empréstimos</h1>
+            <div className="flex gap-6 w-full max-w-4xl mt-4">
+                <CardItem
+                    to="/app/emprestimos"
+                    icon={IconDinheiro}
+                    title="Ver Empréstimos"
+                />
+                <CardItem
+                    to="/app/agiotas"
+                    icon={IconAgiota}
+                    title="Ver Agiotas"
+                />
+                <CardItem
+                    to="/app/solicitar-emprestimo"
+                    icon={IconDinheiro}
+                    title="Solicitar Empréstimo"
+                />
             </div>
         </div>
     );
