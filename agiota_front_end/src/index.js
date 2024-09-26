e8d74c11f1638da107688865c901ff2c580779d4
yasmiinmuniz <yasmindasilvamuniz@gmail.com>
2024-09-25 23:37:02 -0300

    refactor: limpar e organizar o código do index

--- agiota_front_end/src/index.js
+++ agiota_front_end/src/index.js
@@ -2,29 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './styles/index.css';
 import App from './App';
-import Login from './components/login';
-import Cadastro from './components/cadastro/cadastroForm';
-import Home from './pages/home/Home';
-import ListagemEmprestimos from "./pages/emprestimoCliente/listagemEmprestimos";
-import SolicitarEmprestimo from './pages/emprestimoCliente/solicitarEmprestimos';
 import Login from './components/login/FormularioLogin';
 import Cadastro from './components/cadastro/Cadastro';
-import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
-import ListagemEmprestimos from "./pages/emprestimoCliente/listagemEmprestimos";
-import Agiotas from "./pages/agiota/listagemAgiotas";
-import Clientes from "./pages/cliente/listagemClientes";
-import CadastroCliente from "./pages/cliente/cadastroCliente";
+import Home from './pages/home/home';
+import ListagemEmprestimos from './pages/emprestimoCliente/listagemEmprestimos';
+import SolicitarEmprestimo from './pages/emprestimoCliente/solicitarEmprestimos';
+import Agiotas from './pages/agiota/listagemAgiotas';
+import Clientes from './pages/cliente/listagemClientes';
+import CadastroCliente from './pages/cliente/cadastroCliente';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router = createBrowserRouter([
     {
         path: "/app",
-        path: "/app",
         element: <App />,
-        errorElement: <div>Rota inexistente ou com problema!</div>,
         errorElement: <div>Rota inexistente ou com problema!</div>,
         children: [
             {
@@ -43,24 +36,12 @@ const router = createBrowserRouter([
                 path: "emprestimos",
                 element: <ListagemEmprestimos />
             },
-            // {
-            //     path: "agiotas",
-            //     element: <Agiotas />
-            // },
-            // {
-            //     path: "clientes",
-            //     element: <Clientes />
-            // },
-            // {
-            //     path: "cadastrar-cliente",
-            //     element: <CadastroCliente />
-            // },
             {
                 path: "solicitar-emprestimo",
                 element: <SolicitarEmprestimo />
             },
             {
-                path: "/app",
+                path: "listagem-emprestimo",
                 element: <ListagemEmprestimos />
             },
             {
